9c6bcd9a503546ff1b3ccbfd2b4c5391de828c18
aquos <businessgroup77788899@gmail.com>
2026-01-03 13:52:41 +0530

    add implementation

--- Factorial.php
+++ Factorial.php
@@ -8,14 +8,7 @@
 </html>
  <?php
 
- if ($_SERVER["REQUEST_METHOD"]=="POST"){
-    $num= $_POST["number"];
 
-    $factorial = 1;
 
-    for ($i = 1; $i <= $num ; $i++){
-        $factorial *= $i;
-    }
-    echo "$factorial";
- }
+   
  ?>
